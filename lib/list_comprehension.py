3706f165d7f555d99bc492a01894ac7824baabaf
Tamara Mwanzali Kaka <billnyetherussianspy679@gmail.com>
2025-02-12 19:11:38 +0300

    saving changes

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens=[]
+    for nums in num_list:
+        if nums%2==0:
+            evens.append(nums)
+    else:
+        return evens
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_words=[]
+    for words in sentence_list:
+        exclaimed_words.append(words + "!")
+    return exclaimed_words
+
+make_exclamation(["I like computers', 'I require coffee', 'Live long and prosper"])
